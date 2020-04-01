de73dabf48098b15f0d0cf5bd9937be6c02cd07e
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-01 21:35:07 +0900

    Add missing import statement for styled component

--- react-apollo/src/routes/Detail.js
+++ react-apollo/src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
